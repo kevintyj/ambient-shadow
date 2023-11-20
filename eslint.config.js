857aca57670cabf41acf48c5c99de6ddaa11fa4a
kevintyj <hello@kevintyj.com>
2023-11-21 01:56:28 +0530

    feat: Initial vite/solid config

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import antfu from '@antfu/eslint-config';
+
+/* Using custom configuration of Antfu's code style: https://github.com/antfu/eslint-config */
+export default antfu(
+	{
+		typescript: {
+			tsconfigPath: 'tsconfig.json',
+		},
+		stylistic: {
+			indent: 'tab',
+			quotes: 'single',
+		},
+		ignores: [
+			'**/dist',
+			'**/public',
+			'**/__fixtures__',
+			'**.config.{js,ts}',
+			'.idea'
+		],
+		overrides: {
+			typescript: {
+				'ts/consistent-type-definitions': ['error', 'type'],
+				'ts/no-unsafe-assignment': 'warn',
+				'ts/no-unsafe-member-access': 'warn',
+			},
+		},
+	},
+	{
+		files: ['tsconfig.json', 'package.json'],
+		rules: {
+			'jsonc/sort-keys': 'off',
+		},
+	},
+	{
+		rules: {
+			'style/semi': ['error', 'always'],
+		},
+	},
+);
